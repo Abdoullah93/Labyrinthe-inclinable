7c25c03287ddb3aefb2499bdc39575f5fe6d7602
Maximus-Decimu5 <maxime.facchin@laposte.net>
2024-05-29 17:16:02 +0200

    Update forms.h for Plateau class

--- forms.h
+++ forms.h
@@ -3,7 +3,7 @@
 
 #include "geometry.h"
 #include "animation.h"
-
+#include <vector>
 
 class Color
 {
@@ -66,6 +66,17 @@ public:
     Cube_face(Vector v1 = Vector(1,0,0), Vector v2 = Vector(0,0,1),
           Point org = Point(), double l = 1.0, double w = 1.0,
           Color cl = Color());
+    void update(double delta_t);
+    void render();
+};
+
+class Plateau : public Form
+{
+private:
+    std::vector<Cube_face> faces;
+
+public:
+    Plateau(double size = 1.0, Color cl = Color());
     void update(double delta_t);
     void render();
 };
